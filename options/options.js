6b9ecd693d7862d119f4281a3775df64c68d866a
sharafian <ben@sharafian.me>
2021-06-24 23:54:25 -0700

    feat: make the options respond immediately

--- options/options.js
+++ options/options.js
@@ -5,26 +5,6 @@ function valueField (element) {
 	else return 'value'
 }
 
-function saveOptions (e) {
-	e.preventDefault()
-	console.log('saving options')
-
-	const save = document.getElementById('save')
-	save.disabled = true
-
-	const options = {}
-	for (const key of optionNames) {
-		const field = document.getElementById(key)
-		console.log(key, valueField(field))
-		options[key] = field[valueField(field)] 
-	}
-
-	browser.storage.sync.set(options).then(() => {
-		console.log('completed options save;', options)
-		save.disabled = false
-	})
-}
-
 async function restoreOptions () {
 	function setCurrentChoice (option, state) {
 		document.getElementbyId(option).value = state
@@ -40,7 +20,13 @@ async function restoreOptions () {
 	})
 }
 
-restoreOptions().then(() => {
-	document.getElementById('options').addEventListener('submit', saveOptions)
-	console.log('hello world')
-})
+for (const key of optionNames) {
+	const field = document.getElementById(key)
+	field.addEventListener('change', () => {
+		browser.storage.sync.set({
+			[key]: field[valueField(field)]
+		})
+	})
+}
+
+restoreOptions()
